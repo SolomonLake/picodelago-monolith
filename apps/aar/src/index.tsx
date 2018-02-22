e806a07468b4b3bd8c2e7490b64e58f97ec129ef
Lake <lake@peardeck.com>
2018-02-22 00:36:30 -0600

    comment

--- apps/aar/src/index.tsx
+++ apps/aar/src/index.tsx
@@ -7,6 +7,8 @@ require("./index.css");
 
 renderApp();
 
+// -this must be called whenever we want to re-render the app
+// -it will refresh all components, with the new states
 export function renderApp() {
   render(
     <div className="index-window">
