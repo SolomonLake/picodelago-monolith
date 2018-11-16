2d84ff19b1c9f2fcced8a2fea9cf9a4fd2b66d4e
Lake <lake@peardeck.com>
2018-11-16 08:49:11 -0600

    crna ts actually works with jest :)

--- apps/CRNAExpoTSExample/build/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import App from "./App";
-import renderer from "react-test-renderer";
-it("renders without crashing", () => {
-    const rendered = renderer.create(React.createElement(App, null)).toJSON();
-    expect(rendered).toBeTruthy();
-});
-//# sourceMappingURL=App.test.js.map
--- apps/CRNAExpoTSExample/src/App.tsx
+++ apps/CRNAExpoTSExample/src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header/header';
 
 export default class App extends React.Component {
   render() {
+    const { container, header, content } = styles;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={container}>
+        <View style={header}>
+          <Header title="Awesomest App Ever!!!"  />
+        </View>
+        <View style={content}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
       </View>
     );
   }
@@ -14,8 +23,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#eee',
+    justifyContent: 'center'
+  },
+  header: {
+    flex: 1
+  },
+  content: {
+    flex: 6,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
