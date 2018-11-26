b942272c512ff147c645afb49167fa7798dcbc7a
Lake <lake@peardeck.com>
2018-11-25 22:36:26 -0600

    moved the store

--- apps/redux-test/src/App.tsx
+++ apps/redux-test/src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { rootReducer } from "./reducers/rootReducer";
 import { Main } from "./components/Main";
-
-export const store = createStore(rootReducer);
+import { store } from "./store";
 
 export default class App extends React.Component {
   render() {
