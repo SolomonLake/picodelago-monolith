e0934396b7f213665b88e3b207d6696bbc7c1010
Lake <lake@peardeck.com>
2018-12-03 23:42:02 -0600

    save store state on internval, need to load it

--- apps/redux-test/src/store.ts
+++ apps/redux-test/src/store.ts
@@ -1,4 +1,10 @@
 import { createStore } from "redux";
 import { rootReducer } from "./reducers/rootReducer";
+import { setInterval } from "timers";
+import { AsyncStorage } from "react-native";
 
 export const store = createStore(rootReducer);
+
+setInterval(() => {
+  AsyncStorage.setItem("appState", JSON.stringify(store.getState()));
+}, 1000);
